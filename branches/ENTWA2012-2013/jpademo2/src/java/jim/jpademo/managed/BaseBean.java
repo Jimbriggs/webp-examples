a6abf29aa4addee2fc991b46afc97ed99353cf09
jim.briggs <jim.briggs@54ded885-b71f-6dbe-de5e-90ad47c73e33>
2012-11-13 21:10:53 +0000

    Added further error message methods to BaseBean

--- branches/ENTWA2012-2013/jpademo2/src/java/jim/jpademo/managed/BaseBean.java
+++ branches/ENTWA2012-2013/jpademo2/src/java/jim/jpademo/managed/BaseBean.java
@@ -9,8 +9,26 @@ import javax.faces.context.FacesContext;
 
 public class BaseBean {
 
-    protected void addError(String s) {
+    public void addMessage(String clientId, FacesMessage.Severity severity, String summary, String details) {
         FacesContext fc = FacesContext.getCurrentInstance();
-        fc.addMessage(null, new FacesMessage(s));
+        FacesMessage fm = new FacesMessage(severity, summary, details);
+        fc.addMessage(clientId, fm);
     }
+
+    public void addError(String message) {
+        addMessage(null, FacesMessage.SEVERITY_ERROR, message, message);
+    }
+
+    public void addInfo(String message) {
+        addMessage(null, FacesMessage.SEVERITY_INFO, message, message);
+    }
+
+    public void addWarn(String message) {
+        addMessage(null, FacesMessage.SEVERITY_WARN, message, message);
+    }
+
+    public void addFatal(String message) {
+        addMessage(null, FacesMessage.SEVERITY_FATAL, message, message);
+    }
+    
 }
