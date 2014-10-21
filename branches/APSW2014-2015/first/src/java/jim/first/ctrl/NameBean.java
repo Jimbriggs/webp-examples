801afa60114701cbd9495e3be551e4cb65c1d9be
jim.briggs <jim.briggs@54ded885-b71f-6dbe-de5e-90ad47c73e33>
2014-10-21 10:28:45 +0000

    Added a backing bean and a simple user interface

---
+++ branches/APSW2014-2015/first/src/java/jim/first/ctrl/NameBean.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jim.first.ctrl;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author BriggsJ
+ */
+@Named(value = "nameBean")
+@RequestScoped
+public class NameBean {
+
+    /**
+     * Creates a new instance of NameBean
+     */
+    public NameBean() {
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String doName() {
+        return "";
+    }
+
+}
