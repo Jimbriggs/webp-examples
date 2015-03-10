d7f78c36c684e6e93518db197550b5abba31fc21
jim.briggs <jimbriggs@users.noreply.github.com>
2015-03-10 10:18:25 +0000

    2014-2015 version of AjaxExample

---
+++ AjaxExample/AjaxExample/src/java/jim/tiny/exampleBean.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jim.tiny;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.event.AjaxBehaviorEvent;
+
+/**
+ *
+ * @author briggsj
+ */
+@ManagedBean
+@RequestScoped
+public class exampleBean {
+
+    /** Creates a new instance of exampleBean */
+    public exampleBean() {
+    }
+    String text = "Hello ";
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    private Integer length = 0;
+
+    public Integer getLength() {
+//        return text.length();
+        return length;
+    }
+
+    public void countListener(AjaxBehaviorEvent event) {
+        length = text.length();
+    }
+
+    public boolean isShowout() {
+        return (length % 2) == 0;
+    }
+}
