f25aae3d7f0b09ca87c8daf7c85f02251a5b23ca
Jim Briggs <Jim.briggs@port.ac.uk>
2021-04-01 10:15:13 +0100

    Delete buildAndRun.bat

--- jpa2021first/buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t jim/jpa2021first .
-call docker rm -f jpa2021first
-call docker run -d -p 9080:9080 -p 9443:9443 --name jpa2021first jim/jpa2021first
